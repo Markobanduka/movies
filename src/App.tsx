2b9fec077fcca0a486bef74683411f607cb565b5
Markobanduka <banduka44@gmail.com>
2024-07-20 23:50:56 +0200

    added components and basic layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
 import Home from "./components/pages/Home";
+import Footer from "./Templates/Snippets/Footer";
+import Navigation from "./Templates/Snippets/Navigation";
 
 const App = () => {
   return (
-    <div className="flex  h-screen  bg-slate-800">
+    <div className="  h-screen  bg-slate-800">
+      <Navigation />
       <Home />
+      <Footer />
     </div>
   );
 };
