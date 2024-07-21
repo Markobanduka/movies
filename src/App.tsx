93444285a3b53c758c7cd47f32ffaa4d4f11eb53
Markobanduka <banduka44@gmail.com>
2024-07-21 11:11:32 +0200

    search component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,22 @@
-import Home from "./components/pages/Home";
+import { useState } from "react";
+import Movies from "./components/Movies";
+import Search from "./components/Search";
 import Footer from "./Templates/Snippets/Footer";
 import Navigation from "./Templates/Snippets/Navigation";
 
 const App = () => {
+  const [searchInput, setSearchInput] = useState("Terminator");
+  const [query, setQuery] = useState("");
+
+  const handleSearch = () => {
+    setQuery(searchInput);
+  };
+
   return (
     <div className="  h-screen  bg-slate-800">
       <Navigation />
-      <Home />
+      <Search setSearchInput={setSearchInput} handleSearch={handleSearch} />
+      <Movies searchInput={query} />
       <Footer />
     </div>
   );
--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,7 +1,22 @@
-import React from "react";
+interface SearchBarProps {
+  setSearchInput: (input: string) => void;
+  handleSearch: () => void;
+}
 
-const Search = () => {
-  return <div>Search</div>;
+const Search = ({ setSearchInput, handleSearch }: SearchBarProps) => {
+  return (
+    <form className="flex justify-center items-center pt-10">
+      <input
+        type="text"
+        className="input input-primary"
+        placeholder="Search for a movie..."
+        onChange={(e) => setSearchInput(e.target.value)}
+      />
+      <button className="btn btn-primary mx-1" onClick={handleSearch}>
+        Search
+      </button>
+    </form>
+  );
 };
 
 export default Search;
